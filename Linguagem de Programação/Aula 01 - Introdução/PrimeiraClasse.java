9384d4c2b2ff55abe04d31228e4413765ccb28f9
Helder Davidson Rodrigues Alvarenga <70406114+Uken49@users.noreply.github.com>
2022-08-03 14:39:55 -0300

    Adicionando primeiro arquivo de java
    Aula introdutória ao Java da aula Linguagem de Programação

---
+++ Linguagem de Programação/Aula 01 - Introdução/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package com.helder.primeira.aplicacao.java;
+
+// Os comandos tem abreviações para facilitar a programação
+public class PrimeiraClasse {
+
+    // Atalho - psvm
+    public static void main(String[] args) {
+
+        // Atalho - sout
+        System.out.println("Olá mundo");
+    }
+}
