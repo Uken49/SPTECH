a1cd887474d44cf257a4f6d27b62c13822969582
Helder Davidson Rodrigues Alvarenga <70406114+Uken49@users.noreply.github.com>
2022-08-10 11:08:41 -0300

    Adicionando comentários
    Informações sobre atalhos, nomenclaturas e método main

--- Linguagem de Programação/Aula 01 - Introdução/PrimeiraClasse.java
+++ Linguagem de Programação/Aula 01 - Introdução/PrimeiraClasse.java
@@ -1,12 +1,18 @@
 package com.helder.primeira.aplicacao.java;
 
-// Os comandos tem abreviações para facilitar a programação
+// Os comandos tem abreviações/atalhos para facilitar a programação.
+// Os atalhos tem como nome a primeira letra de cada palavra reservada.
+
+// Uma classe em Java tem como padrão o nome em "PascalCase" e sempre no singular, entretanto
+// durante as aulas, poderei estar colocando o nome no plural para fins didático.
 public class PrimeiraClasse {
 
-    // Atalho - psvm
+    // Atalho - psvm ou main
+    // O método "main" é o ponto de partida, é a classe que é executa ao iniciar a aplicação
     public static void main(String[] args) {
 
         // Atalho - sout
+        // Para exibir informações no console utilizaremos o método "println" da Classe System
         System.out.println("Olá mundo");
     }
 }
