dd3062b5b0c17ada2e203457073d0081215678c2
uken49 <heldergavioes@hotmail.com>
2023-02-23 21:34:43 -0300

    add: aula de metodos http

---
+++ 3 Semestre/Técnicas de Programação WEB/Exercicio/login-03221027-helder/src/main/java/school/sptech/login03221027helder/Login03221027HelderApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.login03221027helder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login03221027HelderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Login03221027HelderApplication.class, args);
+	}
+
+}
---
+++ 3 Semestre/Técnicas de Programação WEB/Exercicio/login-03221027-helder/src/test/java/school/sptech/login03221027helder/Login03221027HelderApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.login03221027helder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login03221027HelderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
